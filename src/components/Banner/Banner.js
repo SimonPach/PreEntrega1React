44231e5bf53317fed35888d073b099ecda68463b
simonpachg@gmail.com <simonpachg@gmail.com>
2024-05-09 22:57:14 -0300

    -- PreEntrega CoderHouse Simon Pach --

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css"
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>Tiendulis de Libracos</h1>
+            <p>Mejores precios y formas de pago</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/CartContent/CartContent.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { dataContext } from "../Context/DataContext";
+import CartElements from "./CartElements";
+import CartTotal from "./CartTotal";
+
+import "./CartContent.css";
+
+const CartContent = () => {
+  const { cart } = useContext(dataContext);
+
+  return cart.length > 0 ? (
+  <>
+  <CartElements />
+  <CartTotal />;
+  </> 
+  ) : (
+    <h2 className="cart-message-center">Aun no a seleccionado nada</h2>
+  )
+};
+
+
+
+export default CartContent;
